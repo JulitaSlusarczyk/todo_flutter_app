e342a43237cf49bd5c74fa005e41fb9477b5b473
JulitaSlusarczyk <julietta816@o2.pl>
2022-11-23 12:52:10 +0100

    Show login and number of available free todos

--- lib/TodosPage.dart
+++ lib/TodosPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,7 @@ class TodosPage extends StatefulWidget {
 
 class _TodosPageState extends State<TodosPage> {
   final user = FirebaseAuth.instance.currentUser!;
+  var db = FirebaseFirestore.instance;
 
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
@@ -17,21 +19,37 @@ class _TodosPageState extends State<TodosPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('My TODOs'),
-          actions: [
-            IconButton(
-                onPressed: signOut,
-                icon: const Icon(Icons.logout)
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').where("email", isEqualTo: user.email).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
+        var data = snapshot.data?.docs.first;
+        return Scaffold(
+            appBar: AppBar(
+              title: const Text('My TODOs'),
+              actions: [
+                IconButton(
+                    onPressed: signOut,
+                    icon: const Icon(Icons.logout)
+                )
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: (){},
+            ),
+            body: Column(
+              children: [
+                Center(child: Text("Hello ${data?['login']} ${data?['no_todos']}"))
+              ],
             )
-          ],
-        ),
-        body: Column(
-          children: [
-            Text("Hello ${user.email!}")
-          ],
-        )
+        );
+      }
     );
   }
 }
